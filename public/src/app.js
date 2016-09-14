e7bdc1032526eef645ba07b927b9afc6cb409bdc
Paul Souvestre <psouvestre@genymobile.com>
2016-09-14 17:55:38 +0200

    Gulpfile ready for React.

---
+++ public/src/app.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import jQuery from 'jquery';
+
+class DummyComponent extends React.Component {
+
+    constructor() {
+        super();
+
+    }
+
+    componentWillMount() {
+    }
+
+    render() {
+        return(
+            <div className="col-xs-12">
+                <h1> Start to code bro ! </h1>
+            </div>
+        );
+    }
+
+}
+
+jQuery(function() {
+    ReactDOM.render(
+        <DummyComponent />,
+        document.getElementById('dummy-component')
+    );
+})
