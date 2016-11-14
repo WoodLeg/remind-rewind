eaaf591d842d952d9b9758845e13aed66b9a22ea
Paul Souvestre <psouvestre@genymobile.com>
2016-11-14 12:45:33 +0100

    Fix + menu design.

--- client/public/src/app.const.dev.js
+++ client/public/src/app.const.dev.js
@@ -4,7 +4,7 @@
     angular
     .module('remindRewind')
     .constant('API', {
-        URL: 'http://192.168.15.94:8080/graphql',
+        URL: 'http://192.168.15.94:8080',
         ENDPOINT: {
             SIGNUP:     '/signup',
             SIGNIN:     '/users/signin',
