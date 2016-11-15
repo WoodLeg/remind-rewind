3b564b7508d25fd666033b6b81358699de020aae
Paul Souvestre <psouvestre@genymobile.com>
2016-11-15 15:51:45 +0100

    Added prod file.

---
+++ client/public/src/app.const.prod.js
@@ -0,0 +1,16 @@
+(function() {
+    'use strict';
+
+    angular
+    .module('remindRewind')
+    .constant('API', {
+        URL: 'http://api.remind-rewind.com',
+        ENDPOINT: {
+            SIGNUP:     '/signup',
+            SIGNIN:     '/users/signin',
+            SIGNOUT:    '/signout',
+            GRAPHQL:    '/graphql'
+        }
+    });
+
+}());
