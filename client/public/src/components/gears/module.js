f5ee7e8630b877772cdb0c5400551e35f06aa4de
Paul Souvestre <psouvestre@genymobile.com>
2017-01-20 16:27:23 +0100

    Components.

---
+++ client/public/src/components/gears/module.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export default class GearsComponent extends React.Component {
+    constructor(){
+        super();
+    }
+
+    componentWillMount(){
+
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="col-xs-12">
+                    <h1>Gears component</h1>
+                </div>
+            </div>
+        )
+    }
+
+}
