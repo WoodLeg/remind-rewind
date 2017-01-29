31336468c7316a092b814a0c5b625e504719d503
Paul Souvestre <woodhome@pauls-mbp.home>
2017-01-29 22:17:16 +0100

    Price table component.

---
+++ client/public/src/components/common/price/table.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+
+export default class PrincingTableComponent extends React.Component {
+    constructor() {
+        super();
+    }
+
+    componentWillMount() {
+
+    }
+
+    render() {
+        return (
+            <div className="princing__table col-xs-12">
+
+            </div>
+        );
+    }
+
+}
---
+++ client/public/src/components/footer/module.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default class FooterComponent extends React.Component {
+    constructor() {
+        super();
+    }
+
+    componentWillMount() {
+
+    }
+
+    render() {
+        return (
+            <div className="footer">
+                <h3 className="footer__title">Made by WoodLeg</h3>
+            </div>
+        )
+    }
+
+}
