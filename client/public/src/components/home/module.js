c6e11506be1272c4f42c0774d6e5d8adba62c722
Paul Souvestre <woodhome@pauls-mbp.home>
2017-01-29 22:17:16 +0100

    Price table component.

--- client/public/src/components/home/module.js
+++ client/public/src/components/home/module.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 
 import ContactForm from '../common/form/contact.js';
+import Footer from '../footer/module.js';
 
 export default class HomeComponent extends React.Component {
 
@@ -16,16 +17,20 @@ export default class HomeComponent extends React.Component {
 
     render() {
         return(
-            <div>
+            <div className="home">
                 <div className="col-xs-12 home__header">
                     <div className="home__header-img"></div>
-                    <h1>Remind Rewind Studio</h1>
+                    <div className="col-xs-12 col-sm-6">
+                        <h1 className="home__header-title">Remind Rewind Studio</h1>
+                        <p className="home__header-body">
+                            Un nouveau Home recording studio en proche banlieue parisienne
+                        </p>
+                    </div>
                 </div>
                 <div className="col-xs-12 home__deaf">
                     <div className="col-xs-12 col-sm-6 home__deaf-body">
                         <h1 className="home__deaf-body-title">Deaf Experience ! </h1>
                         <div className="home__deaf-body-content">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                         </div>
                     </div>
@@ -33,8 +38,9 @@ export default class HomeComponent extends React.Component {
                         <div className="home__deaf-img"></div>
                     </div>
                 </div>
-                <div className="col-xs-12 home__gears">
-                    <h1>Gears available</h1>
+                <div className="col-xs-12 home__tarif">
+                    <div className="home__tarif-img"></div>
+                    <h1 className="home__tarif-title">Tarifs</h1>
                 </div>
                 <div className="col-xs-12 home__contact">
                     <div className="home__contact-bg"></div>
