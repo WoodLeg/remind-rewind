1bff258611c3c7d1b8d174a484ba2b8d94a8e41c
Paul Souvestre <psouvestre@genymobile.com>
2017-01-31 18:05:01 +0100

    Pricing WIP.

--- client/public/src/components/common/price/table.js
+++ client/public/src/components/common/price/table.js
@@ -8,14 +8,14 @@ export default class PrincingTableComponent extends React.Component {
     }
 
     componentWillMount() {
-
+        this.deaf = [];
     }
 
     render() {
         return (
             <div className="princing__table col-xs-12">
-                <PriceColumn title="Deaf" price="5" style="col-xs-12 col-sm-3 col-sm-offset-3 column blue"></PriceColumn>
-                <PriceColumn title="Live" price="10" style="col-xs-12 col-sm-3 column red"></PriceColumn>
+                <PriceColumn title="Deaf" price="5" style="col-xs-12 col-sm-6 col-lg-3 col-lg-offset-3 column blue"></PriceColumn>
+                <PriceColumn title="Live" price="10" style="col-xs-12 col-sm-6 col-lg-3 column red"></PriceColumn>
             </div>
         );
     }
