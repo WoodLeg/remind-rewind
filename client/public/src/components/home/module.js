d493d6eb3dc1db8c8206648b59c31ad49c127083
Paul Souvestre <woodhome@pauls-mbp-1.home>
2017-02-04 18:54:34 +0100

    MD responsive.

--- client/public/src/components/home/module.js
+++ client/public/src/components/home/module.js
@@ -21,17 +21,17 @@ export default class HomeComponent extends React.Component {
             <div className="home">
                 <div className="col-xs-12 home__header">
                     <div className="home__header-img"></div>
-                    <div className="col-xs-12 col-sm-12">
+                    <div className="col-xs-12 col-sm-12 col-md-6">
                         <h1 className="home__header-title">Remind Rewind Studio</h1>
                         <p className="home__header-body">
                             Un nouveau Home recording studio en proche banlieue parisienne
                         </p>
-                        <Scrollchor to="#pricing-table" animate={{ offset: -60, duration: 600, ease: 'cubic-bezier(0.645, 0.045, 0.355, 1)' }} className="col-xs-12 col-sm-6 col-sm-offset-3 col-md-4 col-md-offset-4 home__header-body-btn">Voir les prix</Scrollchor>
+                        <Scrollchor to="#pricing-table" animate={{ offset: -60, duration: 600, ease: 'cubic-bezier(0.645, 0.045, 0.355, 1)' }} className="col-xs-12 col-sm-6 col-sm-offset-3 col-md-6 col-md-offset-3 home__header-body-btn">Voir les prix</Scrollchor>
                     </div>
                 </div>
                 <div className="col-xs-12 home__deaf">
                     <div className="home__deaf-img"></div>
-                    <div className="col-xs-12 col-md-6 home__deaf-body">
+                    <div className="col-xs-12 col-md-6 col-md-offset-6 home__deaf-body">
                         <h1 className="home__deaf-body-title">Deaf Experience ! </h1>
                         <div className="home__deaf-body-content">
                             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
