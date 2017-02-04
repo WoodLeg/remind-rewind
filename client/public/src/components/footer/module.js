a3ae91b6b16737a15b86dc98642c4f85c842a18e
Paul Souvestre <woodhome@pauls-mbp-1.home>
2017-02-04 20:15:39 +0100

    Footer.

--- client/public/src/components/footer/module.js
+++ client/public/src/components/footer/module.js
@@ -11,8 +11,10 @@ export default class FooterComponent extends React.Component {
 
     render() {
         return (
-            <div className="footer">
-                <h3 className="footer__title">Made by WoodLeg</h3>
+            <div className="col-xs-12 footer">
+                <h3 className="footer__title">
+                    Coded by Paul Souvestre - Powered by React <img src="/assets/img/react-logo.png" /> and MobX <img src="/assets/img/mobx.png" /> with GraphQL <img src="/assets/img/graphql.png" /> & NodeJS <img src="/assets/img/nodejs.png" />
+                </h3>
             </div>
         )
     }
