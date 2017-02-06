a96a9e36922b3828bcf17a613742fb3520ce0346
Paul Souvestre <psouvestre@genymobile.com>
2017-02-06 13:47:43 +0100

    gulpfile serve reload react app + jam.

--- client/public/src/components/jam/module.js
+++ client/public/src/components/jam/module.js
@@ -11,10 +11,8 @@ export default class JamComponent extends React.Component {
 
     render() {
         return (
-            <div>
-                <div className="col-xs-12">
-                    <h1>Jam component</h1>
-                </div>
+            <div className="jam col-xs-12">
+                <h1>Jam component</h1>
             </div>
         )
     }
