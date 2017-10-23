ca309f5e1be1e35d75246f1d728c274cc8732278
Paul Souvestre <woodhome@pauls-macbook-pro.home>
2017-10-23 12:15:58 +0200

    Removed nodejs serverjs, starting GO server.

--- server/components/contact/controller.js
+++
@@ -1,9 +0,0 @@
-export default{
-    post: function(request, response) {
-        console.log('Contact action');
-        console.log(request.body);
-        setTimeout(function() {
-            response.status(200);
-        }, 3000);
-    }
-}
